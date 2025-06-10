1716061262aa63651bac5a7d74672bbaa384f375
Thunlos <43917783+Poul0s@users.noreply.github.com>
2025-06-10 13:57:52 +0200

    optimize unnecessary usleep when test ended before end of timeout (#16)
    * optimise unessecary usleep when test ended before end of timeout
    
    * remove unecessary include
    
    ---------
    
    Co-authored-by: Paul Salame <psalame@2B5.42angouleme.fr>

--- utils/gnl.hpp
+++ utils/gnl.hpp
@@ -4,19 +4,25 @@
 
 //Don't do that at home
 #define TEST(x) { \
-	int status = 0;   \
-	int test = fork();\
+	int status = 0; \
+	int test = fork(); \
 	if (test == 0) { \
-		x               \
-		showLeaks();    \
+		x showLeaks(); \
 		exit(EXIT_SUCCESS); \
-	} else {          \
-		usleep(TIMEOUT_US); \
-		if (waitpid(test, &status, WNOHANG) == 0) { \
+	} else { \
+		int nb_sleep = 0; \
+		const int usleep_time = TIMEOUT_US / 100; \
+		while (waitpid(test, &status, WNOHANG) == 0 && nb_sleep * usleep_time <= TIMEOUT_US) \
+		{ \
+			usleep(usleep_time); \
+			nb_sleep++; \
+		} \
+		if (waitpid(test, &status, WNOHANG) == 0) \
+		{ \
 			kill(test, 9); \
-			cout << FG_RED << "TIMEOUT";\
-		}             \
-	}                 \
+			cout << FG_RED << "TIMEOUT"; \
+		} \
+	} \
 }
 
 void gnl(int fd, char const * s);
